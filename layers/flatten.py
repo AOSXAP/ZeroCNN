511845a1b151811c1696cadd271c37cdfbc9eb6d
Andrei Piscoran <andrei.piscoran@klusai.com>
2025-07-04 16:51:27 +0300

    Flatten Layer

---
+++ layers/flatten.py
@@ -0,0 +1,12 @@
+'''
+Flattens the input into a 1D vector.
+'''
+
+class FlattenLayer:
+    def __init__(self, input):
+        self.input = input
+        self.input_height = len(input)
+        self.input_width = len(input[0])
+
+    def forward(self):
+        return [item for sublist in self.input for item in sublist]
